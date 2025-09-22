bc74209241417ba2ace354fc828648d3d2fd09b5
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-09-22 20:52:59 +0900

    [add]deviseviewの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
     gem "sqlite3", ">1.7", "<1.8"
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
 
