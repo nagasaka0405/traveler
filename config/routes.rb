44ebf2e326cb4dd229290728fdec5373310608e4
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-09-28 09:31:19 +0900

    [add]ユーザー画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :user, only:[:index, :show]
   resources :posts do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
