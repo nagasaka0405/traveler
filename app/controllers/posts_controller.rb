10bebd531abe41f2e1abad66ecc0efee8661e39e
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-04 08:58:23 +0900

    [add]バリデーション機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,16 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:user_id, :location, :text, :image)
