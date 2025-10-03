9a2d44ae33cac14d80dc18f0018299cd38d48e66
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-04 08:31:52 +0900

    [add]フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(8).reverse_order
-    @following_users = @user.following_users
+    @following_users = @user.following_user
     @follower_users = @user.follower_user
   end
 
