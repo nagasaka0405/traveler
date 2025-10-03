7ea5da4c30cdf23f17d144af3630d1dc7a39ed99
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-04 08:45:11 +0900

    [add]権限機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     gem "sqlite3", ">1.7", "<1.8"
 
+    before_action :authenticate_user!,except: [:top]
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
