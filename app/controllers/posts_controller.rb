d9a0c30bfc931c3d23efdfc06ced88039c111f8f
nagasaka0405 <226518686+nagasaka0405@users.noreply.github.com>
2025-10-04 09:45:56 +0900

    [fix]エラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to post_path(@post.id)
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
